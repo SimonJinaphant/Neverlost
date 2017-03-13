aee76cb8fc529c774f968843b8b499c50c626008
Simon Jinaphant <SimonJinaphant@users.noreply.github.com>
2017-03-12 22:00:05 -0700

    Add downstream messaging from FCM (#7)
    Resolve #4 - Add downstream message from FCM
    
    * Created README with project details and build errors
    * Updated README with Firebase Cloud Messaging info

---
+++ app/src/main/java/com/neverlost/ubc/neverlost/NeverlostFirebaseMessagingService.java
@@ -0,0 +1,71 @@
+package com.neverlost.ubc.neverlost;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class NeverlostFirebaseMessagingService extends FirebaseMessagingService {
+
+    private static final String TAG = "FirebaseMsgService";
+
+    /**
+     * Called when either a notification or data message is received.
+     *
+     * @param remoteMessage Object representing the message received from Firebase Cloud Messaging.
+     */
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        /**
+         * Data messages are handled in this function regardless of whether the app is in the
+         * foreground or background. (See table above)
+         *
+         * Notification messages are only received in this function if the app is in the foreground.
+         * Otherwise an Android notification will be auto-generated and displayed; when the user
+         * taps on the notification they will relaunch the app.
+         */
+
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+
+        // Check if message contains a data payload.
+        if (remoteMessage.getData().size() > 0) {
+            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+        }
+
+        // Check if message contains a notification payload.
+        if (remoteMessage.getNotification() != null) {
+            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+            sendNotification(remoteMessage.getNotification().getBody());
+        }
+    }
+
+    /**
+     * Create and show a simple notification containing the received FCM message.
+     *
+     * @param messageBody FCM message body received.
+     */
+    private void sendNotification(String messageBody) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.ic_menu_send)
+                .setContentTitle("FCM Message")
+                .setContentText(messageBody)
+                .setAutoCancel(true)
+                .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+                .setContentIntent(PendingIntent.getActivity(this, 0, intent,
+                        PendingIntent.FLAG_ONE_SHOT));
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0, notificationBuilder.build());
+    }
+}
