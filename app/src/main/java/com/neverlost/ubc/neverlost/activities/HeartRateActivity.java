cb63b4ffc04afc1c45dfb4f53bd1f190b69a2b36
Junwei Su <junweisu.working@gmail.com>
2017-03-25 14:59:10 -0700

    this commit: 	-integrated firebase into health modules

--- app/src/main/java/com/neverlost/ubc/neverlost/activities/HeartRateActivity.java
+++ app/src/main/java/com/neverlost/ubc/neverlost/activities/HeartRateActivity.java
@@ -1,5 +1,6 @@
 package com.neverlost.ubc.neverlost.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
@@ -9,7 +10,12 @@ import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
 import com.neverlost.ubc.neverlost.R;
+import com.neverlost.ubc.neverlost.firebase.FirebaseQuery;
+import com.neverlost.ubc.neverlost.firebase.FirebaseRef;
 import com.neverlost.ubc.neverlost.objects.Dependent;
 
 import java.util.ArrayList;
@@ -19,26 +25,42 @@ public class HeartRateActivity extends AppCompatActivity {
     LineChart heartRateGraph;
     TextView trendValue;
 
-    //demo user
-    Dependent demouser;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_heart_rate);
+        Intent intent = getIntent();
+        final String uname = intent.getStringExtra("key");
 
+        //get layout elements
         heartRateGraph = (LineChart) findViewById(R.id.heartRateGraph);
         trendValue = (TextView) findViewById(R.id.trendValue);
 
-        ArrayList<Entry> graphEntries = new ArrayList<>();
-        graphEntries.add(new BarEntry(1, 65));
-        graphEntries.add(new BarEntry(2, 78));
-        graphEntries.add(new BarEntry(3, 80));
+        FirebaseRef.dependentRer.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Dependent dependent = FirebaseQuery.getDependent(uname, dataSnapshot);
 
-        LineDataSet lineDataSet = new LineDataSet(graphEntries, "Heart Rate");
-        LineData theData = new LineData(lineDataSet);
-        heartRateGraph.setData(theData);
+                ArrayList<Entry> graphEntries = new ArrayList<>();
 
-        trendValue.setText("Increasing");
+                for(int i=0; i<5; i++){
+                    graphEntries.add(new BarEntry(i,dependent.heartRates.get(i).heartRate));
+                }
+
+                LineDataSet lineDataSet = new LineDataSet(graphEntries, "Heart Rate");
+                LineData theData = new LineData(lineDataSet);
+                heartRateGraph.setData(theData);
+
+                //todo: need a function to detect the trend
+                trendValue.setText("Increasing");
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
     }
 }
