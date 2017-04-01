b87a84f8e3444e6fcedd5f6c54ba6bd298e3c8ef
aaronso <aaron-so@hotmail.com>
2017-03-31 17:07:43 -0700

    Add Splash page, UI for Dependent and Caretaker health pages (#34)

---
+++ app/src/main/java/com/neverlost/ubc/neverlost/activities/MonitorActivity.java
@@ -0,0 +1,15 @@
+package com.neverlost.ubc.neverlost.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.neverlost.ubc.neverlost.R;
+
+public class MonitorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_monitor);
+    }
+}
