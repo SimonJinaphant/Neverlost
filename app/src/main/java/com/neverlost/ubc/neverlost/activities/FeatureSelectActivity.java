acf95181ea36206083fd87ba199654b2a261fd56
aaronso <aaron-so@hotmail.com>
2017-04-04 17:13:02 -0700

    splash page, UI for dependent and caretaker health pages (#44)

--- app/src/main/java/com/neverlost/ubc/neverlost/activities/FeatureSelectActivity.java
+++ app/src/main/java/com/neverlost/ubc/neverlost/activities/FeatureSelectActivity.java
@@ -3,12 +3,10 @@ package com.neverlost.ubc.neverlost.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import com.neverlost.ubc.neverlost.R;
-import com.neverlost.ubc.neverlost.models.readData;
 
 public class FeatureSelectActivity extends AppCompatActivity {
 
