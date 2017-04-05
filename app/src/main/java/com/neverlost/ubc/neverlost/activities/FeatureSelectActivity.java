412c9790086c3b48fcd352d214fac3f06946f86b
SimonJinaphant <SimonJinaphant@users.noreply.github.com>
2017-04-05 14:44:41 -0700

    Merge with master

--- app/src/main/java/com/neverlost/ubc/neverlost/activities/FeatureSelectActivity.java
+++ app/src/main/java/com/neverlost/ubc/neverlost/activities/FeatureSelectActivity.java
@@ -84,7 +84,7 @@ public class FeatureSelectActivity extends AppCompatActivity {
         mapButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(FeatureSelectActivity.this, MainActivity.class);
+                Intent intent = new Intent(FeatureSelectActivity.this, MapActivity.class);
                 startActivity(intent);
             }
         });
